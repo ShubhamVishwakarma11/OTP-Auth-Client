ab0b833b0f3a35987fac285b8ec3280b2bfeb26b
Shubham Vishwakarma <shubham.vish.2633@gmail.com>
2023-06-04 23:42:14 +0530

    ui: add signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -8,14 +8,14 @@ const Login = () => {
     <div className='w-full flex justify-center items-center'>
         <div className="w-30% bg-blue-100 flex flex-col justify-between items-center m-12 p-12 rounded-xl">
             <div className="w-full flex flex-col justify-start items-start">
-              <h1 className='text-4xl font-black'>Login</h1>
-              <p className='text-xl font-medium text-slate-500'>Please sign in to continue</p>
+              <h1 className='text-4xl font-black'>Signup</h1>
+              <p className='text-xl font-medium text-slate-500'>Please sign up to continue</p>
             </div> 
             <div className="mt-12 p-6 rounded-full bg-blue-400">
               <MdEmail className='text-white text-7xl'/>
             </div>
             <LoginForm />
-            <p className='text-md mt-2 text-slate-500'> No Account? <Link href="/signup">Create One</Link></p>
+            <p className='text-md text-slate-500 mt-2'> Already have an Account? <Link href="/login">Login Here</Link></p>
         </div>
     </div>
   )
